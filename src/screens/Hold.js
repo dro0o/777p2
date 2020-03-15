12cb01faf7d521424fc0497678721db22db19601
Andrew Pittman <andrew@pittman.dev>
2020-03-15 11:47:59 -0400

    build app.js

--- src/screens/Hold.js
+++
@@ -1,3 +0,0 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
