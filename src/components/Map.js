1c65413ad4fcf7ba2d9b973e2628c07b5c89e872
Andrew Pittman <andrew@pittman.dev>
2020-03-29 01:00:06 -0400

    add all layers in

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,6 +10,11 @@ import MapView, {
 var { vw, vh, vmin, vmax } = require('react-native-viewport-units')
 import { Context as LocationContext } from '../context/LocationContext'
 import { Context as LayerContext } from '../context/LayerContext'
+import parkingImg from '../img/parking.png'
+import peakImg from '../img/peak.png'
+import poiImg from '../img/poi.png'
+import roadImg from '../img/roads.png'
+import trailImg from '../img/trail.png'
 
 const Map = ({ centerUser, followUser }) => {
   const {
@@ -56,61 +61,19 @@ const Map = ({ centerUser, followUser }) => {
       : null
   }, [currentLocation])
 
-  // attempt to build parking arrays to bypass view poly callout issue
-  // useEffect(() => {
-  //   try {
-  //     var parkingPolys = []
-  //     var parkingMarkers = []
-  //     !parking
-  //       ? null
-  //       : data.parking.map(el => {
-  //           for (const [ind, poly] of el.location.coordinates.entries()) {
-  //             parkingPolys.push(
-  //               <Polygon
-  //                 // key={el._id.concat(ind.toString(), 'poly')}
-  //                 coordinates={poly[0].map(coords => ({
-  //                   latitude: coords[1],
-  //                   longitude: coords[0]
-  //                 }))}
-  //                 strokeColor='#000'
-  //                 fillColor='rgba(255,0,0,0.5)'
-  //                 strokeWidth={1}
-  //                 tappable={true}
-  //                 onPress={() => {
-  //                   toggle(poly)
-  //                 }}
-  //               />
-  //             )
-  //             parkingMarkers.push(
-  //               <Marker
-  //                 ref={ref => {
-  //                   poly.marker = ref
-  //                   // poly.open = false
-  //                 }}
-  //                 coordinate={{
-  //                   latitude: poly[0][0][1],
-  //                   longitude: poly[0][0][0]
-  //                 }}
-  //               >
-  //                 <Callout>
-  //                   <Text>Hello</Text>
-  //                 </Callout>
-  //               </Marker>
-  //             )
-  //           }
-  //         })
-  //   } catch (err) {
-  //     var parkingPolys = []
-  //     var parkingMarkers = []
-  //     console.log('parking error', err)
-  //   }
-  // }, [data])
-
   if (!currentLocation) {
     return <ActivityIndicator size='large' style={{ marginTop: 200 }} />
   }
 
   console.log(centerUser, followUser, typeof currentLocation.coords.latitude)
+
+  // Function to handle toggling each non marker geo element's callout
+  function toggle(el) {
+    // el.marker.showCallout()
+    console.log(el.open)
+    el.open ? el.marker.hideCallout() : el.marker.showCallout()
+    el.open = !el.open
+  }
 
   function Boundary() {
     try {
@@ -132,97 +95,27 @@ const Map = ({ centerUser, followUser }) => {
       return null
     }
   }
-
-  // parking
-  // var parkingPolys = []
-  // var parkingMarkers = []
-  // try {
-  //   return !parking
-  //     ? null
-  //     : data.parking.map(el => {
-  //         for (const [ind, poly] of el.location.coordinates.entries()) {
-
-  //           parkingPolys.push(
-  //               <Polygon
-  //                 // key={el._id.concat(ind.toString(), 'poly')}
-  //                 coordinates={poly[0].map(coords => ({
-  //                   latitude: coords[1],
-  //                   longitude: coords[0]
-  //                 }))}
-  //                 strokeColor='#000'
-  //                 fillColor='rgba(255,0,0,0.5)'
-  //                 strokeWidth={1}
-  //                 tappable={true}
-  //                 onPress={() => {
-  //                   toggle(poly)
-  //                 }}
-  //               />
-  //           )
-  //           parkingMarkers.push(
-  //               <Marker
-  //                 ref={ref => {
-  //                   poly.marker = ref
-  //                   // poly.open = false
-  //                 }}
-  //                 coordinate={{
-  //                   latitude: poly[0][0][1],
-  //                   longitude: poly[0][0][0]
-  //                 }}
-  //               >
-  //                 <Callout>
-  //                   <Text>Hello</Text>
-  //                 </Callout>
-  //               </Marker>
-  //           )
-  //         }
-  //       })
-  // } catch (err) {
-  //   return null
-  // }
-
-  function toggle(el) {
-    el.open ? el.marker.hideCallout() : el.marker.showCallout()
-    el.open = !el.open
-  }
-
-  function Parking() {
+  function ParkingPolygons() {
     try {
       return !parking
         ? null
         : data.parking.map(el => {
             for (const [ind, poly] of el.location.coordinates.entries()) {
-              console.log(poly[0][0][1])
               return (
-                <View key={el._id.concat(ind.toString(), 'view')}>
-                  <Polygon
-                    // key={el._id.concat(ind.toString(), 'poly')}
-                    coordinates={poly[0].map(coords => ({
-                      latitude: coords[1],
-                      longitude: coords[0]
-                    }))}
-                    strokeColor='#000'
-                    fillColor='rgba(255,0,0,0.5)'
-                    strokeWidth={1}
-                    tappable={true}
-                    onPress={() => {
-                      toggle(poly)
-                    }}
-                  />
-                  <Marker
-                    ref={ref => {
-                      poly.marker = ref
-                      // poly.open = false
-                    }}
-                    coordinate={{
-                      latitude: poly[0][0][1],
-                      longitude: poly[0][0][0]
-                    }}
-                  >
-                    <Callout>
-                      <Text>Hello</Text>
-                    </Callout>
-                  </Marker>
-                </View>
+                <Polygon
+                  key={el._id.concat(ind.toString(), 'poly')}
+                  coordinates={poly[0].map(coords => ({
+                    latitude: coords[1],
+                    longitude: coords[0]
+                  }))}
+                  strokeColor='#000'
+                  fillColor='rgba(255,0,0,0.5)'
+                  strokeWidth={1}
+                  // tappable={true}
+                  onPress={() => {
+                    toggle(poly)
+                  }}
+                />
               )
             }
           })
@@ -230,20 +123,219 @@ const Map = ({ centerUser, followUser }) => {
       return null
     }
   }
-  function Peaks(peaks) {
-    return !peaks ? null : null
+  function ParkingMarkers() {
+    try {
+      return !parking
+        ? null
+        : data.parking.map(el => {
+            for (const [ind, poly] of el.location.coordinates.entries()) {
+              return (
+                <Marker
+                  key={el._id.concat(ind.toString(), 'marker')}
+                  ref={ref => {
+                    poly.marker = ref
+                    poly.open = false
+                  }}
+                  coordinate={{
+                    latitude: poly[0][0][1],
+                    longitude: poly[0][0][0]
+                  }}
+                  centerOffset={{ x: -0, y: -5 }}
+                  anchor={{ x: 0.69, y: 1 }}
+                  image={parkingImg}
+                >
+                  <Callout>
+                    <Text>{el.name}</Text>
+                  </Callout>
+                </Marker>
+              )
+            }
+          })
+    } catch (err) {
+      return null
+    }
   }
-  function POIs(pois) {
-    return !pois ? null : null
+  function Peaks() {
+    try {
+      return !peaks
+        ? null
+        : data.peaks.map(el => {
+            return (
+              <Marker
+                key={el._id.concat('peakMarker')}
+                coordinate={{
+                  latitude: el.location.coordinates[1],
+                  longitude: el.location.coordinates[0]
+                }}
+                centerOffset={{ x: -0, y: -5 }}
+                anchor={{ x: 0.69, y: 1 }}
+                image={peakImg}
+              >
+                <Callout>
+                  <Text>{el.name}</Text>
+                </Callout>
+              </Marker>
+            )
+          })
+    } catch (err) {
+      console.log(err)
+      return null
+    }
   }
-  function Roads(roads) {
-    return !roads ? null : null
+  function POIs() {
+    try {
+      return !pois
+        ? null
+        : data.poi.map(el => {
+            return (
+              <Marker
+                key={el._id.concat('poiMarker')}
+                coordinate={{
+                  latitude: el.location.coordinates[1],
+                  longitude: el.location.coordinates[0]
+                }}
+                centerOffset={{ x: -0, y: -5 }}
+                anchor={{ x: 0.69, y: 1 }}
+                image={poiImg}
+              >
+                <Callout>
+                  <Text>{el.name}</Text>
+                  <Text>{el.properties.POITYPE}</Text>
+                </Callout>
+              </Marker>
+            )
+          })
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+  }
+  function RoadPolylines() {
+    try {
+      return !roads
+        ? null
+        : data.roads.map(el => {
+            for (const [ind, line] of el.location.coordinates.entries()) {
+              return (
+                <Polyline
+                  key={el._id.concat(ind.toString(), 'roadsPoly')}
+                  coordinates={line.map(coords => ({
+                    latitude: coords[1],
+                    longitude: coords[0]
+                  }))}
+                  strokeColor='#34495E'
+                  strokeWidth={1.5}
+                  lineJoin='bevel'
+                  tappable={true}
+                  onPress={() => {
+                    toggle(line)
+                  }}
+                />
+              )
+            }
+          })
+    } catch (err) {
+      return null
+    }
+  }
+
+  function RoadMarkers() {
+    try {
+      return !roads
+        ? null
+        : data.roads.map(el => {
+            for (const [ind, line] of el.location.coordinates.entries()) {
+              return (
+                <Marker
+                  key={el._id.concat(ind.toString(), 'roadsMarker')}
+                  ref={ref => {
+                    line.marker = ref
+                    line.open = false
+                  }}
+                  coordinate={{
+                    latitude: line[0][1],
+                    longitude: line[0][0]
+                  }}
+                  centerOffset={{ x: -0, y: -5 }}
+                  anchor={{ x: 0.69, y: 1 }}
+                  image={roadImg}
+                >
+                  <Callout>
+                    <Text>{el.name}</Text>
+                  </Callout>
+                </Marker>
+              )
+            }
+          })
+    } catch (err) {
+      return null
+    }
   }
   function Tracks(tracks) {
     return !tracks ? null : null
   }
-  function Trails(trails) {
-    return !trails ? null : null
+  function TrailPolylines() {
+    try {
+      return !trails
+        ? null
+        : data.trails.map(el => {
+            for (const [ind, line] of el.location.coordinates.entries()) {
+              return (
+                <Polyline
+                  key={el._id.concat(ind.toString(), 'trailsLine')}
+                  coordinates={line.map(coords => ({
+                    latitude: coords[1],
+                    longitude: coords[0]
+                  }))}
+                  strokeColor='#2D482E'
+                  strokeWidth={1.5}
+                  lineJoin='bevel'
+                  lineDashPhase={2}
+                  lineDashPattern={[2, 3, 2]}
+                  tappable={true}
+                  onPress={() => {
+                    toggle(line)
+                  }}
+                />
+              )
+            }
+          })
+    } catch (err) {
+      return null
+    }
+  }
+
+  function TrailMarkers() {
+    try {
+      return !trails
+        ? null
+        : data.trails.map(el => {
+            for (const [ind, line] of el.location.coordinates.entries()) {
+              return (
+                <Marker
+                  key={el._id.concat(ind.toString(), 'trailsMarker')}
+                  ref={ref => {
+                    line.marker = ref
+                    line.open = false
+                  }}
+                  coordinate={{
+                    latitude: line[0][1],
+                    longitude: line[0][0]
+                  }}
+                  centerOffset={{ x: -0, y: -5 }}
+                  anchor={{ x: 0.69, y: 1 }}
+                  image={trailImg}
+                >
+                  <Callout>
+                    <Text>{el.name}</Text>
+                  </Callout>
+                </Marker>
+              )
+            }
+          })
+    } catch (err) {
+      return null
+    }
   }
 
   // Shows user's location as small purple circle
@@ -272,14 +364,15 @@ const Map = ({ centerUser, followUser }) => {
       }}
     >
       <Boundary />
-      {/* <Parking /> */}
-      {parkingPolys}
-      {parkingMarkers}
+      <ParkingPolygons />
+      <ParkingMarkers />
       <Peaks />
       <POIs />
-      <Roads />
+      <RoadPolylines />
+      <RoadMarkers />
       <Tracks />
-      <Trails />
+      <TrailPolylines />
+      <TrailMarkers />
       <UserLoc />
       <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
